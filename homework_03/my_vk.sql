dd17f8cfea9e9be555d8018b1e7a3396eb19c328
Alexander Kolotov <amkolotov@mail.ru>
2020-03-29 19:41:20 +0500

    homework_03

---
+++ homework_03/my_vk.sql
@@ -0,0 +1,170 @@
+-- Создаем базу данных
+CREATE DATABASE my_vk;
+
+-- Делаем ее текущей
+USE my_vk;
+
+-- Создаем таблицу пользователей
+CREATE TABLE users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  email VARCHAR(120) NOT NULL UNIQUE,
+  phone VARCHAR(120) NOT NULL UNIQUE,
+  created_at DATETIME DEFAULT NOW(),
+  update_at DATETIME DEFAULT NOW() ON UPDATE NOW()
+);
+
+-- Таблица профилей
+CREATE TABLE profiles (
+  user_id INT UNSIGNED NOT NULL PRIMARY KEY,
+  gender CHAR(1) NOT NULL,
+  birthday DATE,
+  city VARCHAR(100),
+  photo_id INT UNSIGNED NOT NULL,
+  created_at DATETIME DEFAULT NOW(),
+  update_at DATETIME DEFAULT NOW() ON UPDATE NOW()
+);
+
+-- Таблица сообщений
+CREATE TABLE messages (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  from_user_id INT UNSIGNED NOT NULL,
+  to_user_id INT UNSIGNED NOT NULL,
+  body TEXT NOT NULL,
+  is_important BOOLEAN,
+  is_delivered BOOLEAN,
+  created_at DATETIME DEFAULT NOW()
+);
+
+-- Таблица дружбы
+CREATE TABLE friendship (
+  user_id INT UNSIGNED NOT NULL,
+  friend_id INT UNSIGNED NOT NULL,
+  status_id INT UNSIGNED NOT NULL,
+  requested_at DATETIME DEFAULT NOW(),
+  confirmed_at DATETIME,
+  PRIMARY KEY (user_id, friend_id)
+);
+
+-- Таблица статусов дружеских отношений
+CREATE TABLE friendship_statuses (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(150) NOT NULL UNIQUE
+);
+
+-- Таблица групп
+CREATE TABLE communities (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(150) NOT NULL UNIQUE
+);
+-- Таблица связи пользователей и групп
+CREATE TABLE communutues_users (
+  community_id INT UNSIGNED NOT NULL,
+  user_id iNT UNSIGNED NOT NULL,
+  PRIMARY KEY (community_id, user_id)
+);
+
+-- Таблица медиафайлов
+CREATE TABLE media (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  media_type_id INT UNSIGNED NOT NULL,
+  user_id INT UNSIGNED NOT NULL,
+  file_path VARCHAR(255) NOT NULL,
+  size INT NOT NULL,
+  metadata JSON,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  update_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Таблица типов медиафайлов
+CREATE TABLE media_types (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
